93a6eddaa2c40abe7c759889c444759492484520
Fisherman166 <seankoppenhafer@gmail.com>
2014-11-14 20:15:56 -0800

    Can now decode L1 writes and reads

--- f_proj.c
+++ f_proj.c
@@ -57,10 +57,13 @@ typedef struct TLINE{
 	uint8_t  dec_op;
 }TLINE;
 
-/* Functions */
+/* Trace related functions */
 TLINE * get_line(int * index);
 void decode_trace(uint8_t trace_op, uint32_t address);
 uint8_t check_tags(uint16_t tag, cache_set* set);
+
+/* CPU reads and writes */
+void L1_read_or_write(uint8_t CPU_op, uint16_t tag, uint32_t address, uint8_t tag_matched_line, cache_set* indexed_set);
 void fill_invalid_line(uint8_t CPU_op, uint16_t tag, uint32_t address, cache_set* set);
 void fill_valid_line(uint8_t CPU_op, uint16_t tag, uint32_t address, cache_set* set);
 
@@ -74,35 +77,36 @@ FILE *trace;
 TLINE trace_line;
 cache_set sets[NUM_SETS];	//8k sets in the cache - see cache.h
 
-///* Sean's test main */
-//int main() {
-//	uint32_t address = 0x60680000;
-//	int set_index, line_index;
-//
-//	/*INITIALIZE CACHE ARRAY*/
-//	for(set_index = 0; set_index < NUM_SETS; set_index++) {
-//		for(line_index = 0; line_index < WAYS; line_index++){
-//			sets[set_index].psuedo_LRU = 0;
-//			sets[set_index].valid_ways = 0;
-//
-//			/* Init values in each line */
-//			sets[set_index].line[line_index].tag = 0;
-//			sets[set_index].line[line_index].MESIF = INVALID;
-//
-//		}/*END INNER FOR*/
-//
-//	}/*END OUTER FOR*/
-//
-//	for(set_index = 0; set_index < 20; set_index++) {
-//		decode_trace(READ_DATA_L1, address);
-//		printf("Lines = %d\n\n", sets[0].valid_ways);
-//		address += 0x01000000;
-//	}
-//		address -= 0x01000000;
-//	decode_trace(READ_DATA_L1, address);
-//
-//	return 0;
-//}
+/* Sean's test main */
+int main() {
+	uint32_t address = 0x60680000;
+	int set_index, line_index;
+
+	/*INITIALIZE CACHE ARRAY*/
+	for(set_index = 0; set_index < NUM_SETS; set_index++) {
+		for(line_index = 0; line_index < WAYS; line_index++){
+			sets[set_index].psuedo_LRU = 0;
+			sets[set_index].valid_ways = 0;
+
+			/* Init values in each line */
+			sets[set_index].line[line_index].tag = 0;
+			sets[set_index].line[line_index].MESIF = INVALID;
+
+		}/*END INNER FOR*/
+
+	}/*END OUTER FOR*/
+
+	
+	for(set_index = 0; set_index < 20; set_index++) {
+		decode_trace(WRITE_DATA_L1, address);
+		printf("Lines = %d\n\n", sets[0].valid_ways);
+		address += 0x01000000;
+	}
+		address -= 0x01000000;
+	decode_trace(READ_DATA_L1, address);
+
+	return 0;
+}
 
 /******************************************************************************
  * 	MAIN LOOP: 
@@ -112,80 +116,80 @@ cache_set sets[NUM_SETS];	//8k sets in the cache - see cache.h
  * 	-UPDATE STATUS
  *****************************************************************************/
 
-int main (int argc, char * argv[])
-{
-	int index=0;
-	int bufx=2;
-	//char * next_line;
-	TLINE * next_line;
-	char * SnoopResult;
-	uint32_t Address;
-
-	int set_index, line_index;
-
-	time_t t;
-	srand(time(&t));
-
-	#ifdef TEST_FILE
-		if(argc>1){
-			trace = fopen(argv[1], "r");
-		}else{
-			printf("\nplease specify trace file: \"./a.out <file name> \"\n\n");
-			exit(-1);
-		}
-	#else
-		trace = fopen("cc1.din", "r");
-	#endif
-		/*INITIALIZE CACHE ARRAY*/
-		for(set_index = 0; set_index < NUM_SETS; set_index++) {
-			for(line_index = 0; line_index < WAYS; line_index++){
-				sets[set_index].psuedo_LRU = 0;
-				sets[set_index].valid_ways = 0;
-
-				/* Init values in each line */
-				sets[set_index].line[line_index].tag = 0;
-				sets[set_index].line[line_index].MESIF = INVALID;
-
-				#ifdef WARM_CACHE
-					sets[set_index].line[line_index].MESIF = (rand()%4);
-					sets[set_index].line[line_index].tag = (rand()%exp(16,32));
-				#endif
-			}/*END INNER FOR*/
-
-		}/*END OUTER FOR*/
-
-		if(trace == NULL){ 		/*FILE FAILED TO OPEN*/
-			printf("UNABLE TO OPEN FILE\n");
-			exit(-1); 		/*EXIT WITH ERROR*/
-		}else printf("FILE opened successfully!\n");
-
-		do{ 	/*DONT WANT TO CHECK PRIOR TO */
-			next_line = get_line(&index);	
-			++index;
-		
-			#ifndef SILENT
-				printf("CONTROL: %c\n", next_line->snooped[0]);	
-				printf("STRING RESULT from MAIN: %s\n", next_line->snooped);	
-				printf("MAIN next_line->address: %s\n", next_line->address);
-			#endif
-		//	while((bufx < strlen(next_line->snooped)) && isalnum(next_line->snooped[bufx])){
-		//		next_line->address[bufx] = next_line->snooped[bufx];
-		//	printf("MAIN next_line->address[bufx]: %c\n", next_line->address[bufx]);
-		//	++bufx;
-		//	}
-			//sscanf(next_line->address, "%s", &next_line->dec_addr);
-			//next_line->dec_addr = (long int)strtol(next_line->address, NULL, 0);
-			#ifndef SILENT
-			//	printf("decoded address: 0x%x\n",next_line->dec_addr);
-			#endif
-			
-			//trace_line.dec_op = GetSnoopResult(next_line);
-			//check_tag(next_line);
-			//BusOperation(next_line->snooped[0], *next_line->dec_addr);	
-
-		}while(*next_line->snooped != EOF);
-	return 0;
-}/*END MAIN*/
+//int main (int argc, char * argv[])
+//{
+//	int index=0;
+//	int bufx=2;
+//	//char * next_line;
+//	TLINE * next_line;
+//	char * SnoopResult;
+//	uint32_t Address;
+//
+//	int set_index, line_index;
+//
+//	time_t t;
+//	srand(time(&t));
+//
+//	#ifdef TEST_FILE
+//		if(argc>1){
+//			trace = fopen(argv[1], "r");
+//		}else{
+//			printf("\nplease specify trace file: \"./a.out <file name> \"\n\n");
+//			exit(-1);
+//		}
+//	#else
+//		trace = fopen("cc1.din", "r");
+//	#endif
+//		/*INITIALIZE CACHE ARRAY*/
+//		for(set_index = 0; set_index < NUM_SETS; set_index++) {
+//			for(line_index = 0; line_index < WAYS; line_index++){
+//				sets[set_index].psuedo_LRU = 0;
+//				sets[set_index].valid_ways = 0;
+//
+//				/* Init values in each line */
+//				sets[set_index].line[line_index].tag = 0;
+//				sets[set_index].line[line_index].MESIF = INVALID;
+//
+//				#ifdef WARM_CACHE
+//					sets[set_index].line[line_index].MESIF = (rand()%4);
+//					sets[set_index].line[line_index].tag = (rand()%exp(16,32));
+//				#endif
+//			}/*END INNER FOR*/
+//
+//		}/*END OUTER FOR*/
+//
+//		if(trace == NULL){ 		/*FILE FAILED TO OPEN*/
+//			printf("UNABLE TO OPEN FILE\n");
+//			exit(-1); 		/*EXIT WITH ERROR*/
+//		}else printf("FILE opened successfully!\n");
+//
+//		do{ 	/*DONT WANT TO CHECK PRIOR TO */
+//			next_line = get_line(&index);	
+//			++index;
+//		
+//			#ifndef SILENT
+//				printf("CONTROL: %c\n", next_line->snooped[0]);	
+//				printf("STRING RESULT from MAIN: %s\n", next_line->snooped);	
+//				printf("MAIN next_line->address: %s\n", next_line->address);
+//			#endif
+//		//	while((bufx < strlen(next_line->snooped)) && isalnum(next_line->snooped[bufx])){
+//		//		next_line->address[bufx] = next_line->snooped[bufx];
+//		//	printf("MAIN next_line->address[bufx]: %c\n", next_line->address[bufx]);
+//		//	++bufx;
+//		//	}
+//			//sscanf(next_line->address, "%s", &next_line->dec_addr);
+//			//next_line->dec_addr = (long int)strtol(next_line->address, NULL, 0);
+//			#ifndef SILENT
+//			//	printf("decoded address: 0x%x\n",next_line->dec_addr);
+//			#endif
+//			
+//			//trace_line.dec_op = GetSnoopResult(next_line);
+//			//check_tag(next_line);
+//			//BusOperation(next_line->snooped[0], *next_line->dec_addr);	
+//
+//		}while(*next_line->snooped != EOF);
+//	return 0;
+//}/*END MAIN*/
 
 /******************************************************************************
  * USED TO GET NEXT LINE FROM TRACE FILE 
@@ -220,8 +224,6 @@ return &trace_line;
 }/**END GET LINE*/
 
 
-
-
 /******************************************************************************
 * FUNCTION TO DECODE TRACE OPERATION
 ******************************************************************************/
@@ -248,22 +250,15 @@ void decode_trace(uint8_t trace_op, uint32_t address) {
 
 		switch( trace_op ) {
 			case READ_DATA_L1:
+				L1_read_or_write(CPU_READ, tag, address, tag_matched_line, indexed_set);
+				break;
 
-				if( tag_matched_line == no_match ) {	/* Not in the cache already */
-					if( indexed_set-> valid_ways < WAYS) { /* There is an invalid line */
-						fill_invalid_line(CPU_READ, tag, address, indexed_set);
-						printf("IN FILL_INVALID\n");
-					}
-					else { /* All lines are currently valid */
-						fill_valid_line(CPU_READ, tag, address, indexed_set);
-						printf("IN FILL_VALID\n");
-					}
-				}
-				else {	/* In the cache already */
-					indexed_set->psuedo_LRU = update_LRU(tag_matched_line, indexed_set->psuedo_LRU);
-					CPU_operation(CPU_READ, tag, &indexed_set->line[tag_matched_line]);
-						printf("IN CACHE\n");
-				}
+			case WRITE_DATA_L1:
+				L1_read_or_write(CPU_WRITE, tag, address, tag_matched_line, indexed_set);
+				break;
+
+			case READ_INS_L1:
+				L1_read_or_write(CPU_READ, tag, address, tag_matched_line, indexed_set);
 				break;
 		}//End switch
 	} //End if
@@ -293,6 +288,29 @@ uint8_t check_tags(uint16_t tag, cache_set* set) {
 	return match_index;
 }/*END CHECK TAG BIT*/
 
+
+
+/******************************************************************************
+ * DETERMINES WHAT TO DO ON A CPU READ OR WRITE DEPENDING ON IF THE LINE IS
+ * ALREADY IN THE CACHE OR NOT
+ *****************************************************************************/
+void L1_read_or_write(uint8_t CPU_op, uint16_t tag, uint32_t address, 
+							 uint8_t tag_matched_line, cache_set* indexed_set) { 
+	const uint8_t no_match = 0xFF;
+
+	if( tag_matched_line == no_match ) {	/* Not in the cache already */
+		if( indexed_set-> valid_ways < WAYS) { /* There is an invalid line */
+			fill_invalid_line(CPU_op, tag, address, indexed_set);
+		}
+		else { /* All lines are currently valid */
+			fill_valid_line(CPU_op, tag, address, indexed_set);
+		}
+	}
+	else {	/* In the cache already */
+		indexed_set->psuedo_LRU = update_LRU(tag_matched_line, indexed_set->psuedo_LRU);
+		CPU_operation(CPU_op, tag, &indexed_set->line[tag_matched_line]);
+	}
+}
 
 /******************************************************************************
  * FUNCTION THAT FILLS AN INVALID CACHE LINE
@@ -338,6 +356,7 @@ void fill_valid_line(uint8_t CPU_op, uint16_t tag, uint32_t address, cache_set* 
 	set->psuedo_LRU = update_LRU(line_to_evict, set->psuedo_LRU);
 	CPU_operation(CPU_op, address, &set->line[line_to_evict]);
 }
+
 
 /******************************************************************************
  * EXTRACTS THE BYTE SELECT BITS FROM THE ADDRESS
